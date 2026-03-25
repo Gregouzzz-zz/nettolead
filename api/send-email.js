8d5f0061a2025a98013ed8987b8084f96c1ebc98
Gregouzzz-zz <klein.gregory@outlook.fr>
2026-03-25 15:20:03 +0100

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
     },
     body: JSON.stringify({
      from: "NettoLead <contact@nettolead.fr>"
-      to: email,
+     to: "tonemail@gmail.com",
       subject: "🚀 Bienvenue sur NettoLead",
       html: `
         <h2>Bienvenue sur NettoLead 🚀</h2>
